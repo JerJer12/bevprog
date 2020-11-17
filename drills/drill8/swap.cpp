3f7305999ea8676462442688358dbc31959378df
JerJer12 <teremibence@gmail.com>
2020-11-17 18:30:40 +0100

    update

--- drills/drill8/swap.cpp
+++ drills/drill8/swap.cpp
@@ -1,46 +1,59 @@
 #include "std_lib_facilities.h"
 
-void swap_v(int a,int b)	//Ez a függvény tulajdonképpen nem csinál semmit xD
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
+void swap_v(int a, int b)
+	{ int temp; temp = a, a = b; b = temp; }
+
+void swap_r(int& a, int& b)
+	{ int temp; temp = a, a = b; b = temp; }
+
+/*void swap_cr(const int& cx, const int& cy)
+	{ int temp; temp = cx, cx = cy; cy = temp; }*/
+
+void swap_d(double& a, double& b)
+	{ double temp; temp = a, a = b; b = temp; }
+
+void swap_d1(double a, double b)
+	{ double temp; temp = a, a = b; b = temp; }
 
 int main()
 {
 	int x = 7;
-	int y = 9;
+	int y =9;
+	swap_r(x,y);
 
-	cout << "x=" << x << " y=" << y << endl;
-
-	swap_v(x,y);
-
-	cout << "x=" << x << " y=" << y << endl;
+	cout<< x <<endl;
+	cout<< y <<endl;
 
 	swap_v(7,9);
 
-	const int cx = 7;
+	cout<< x <<endl;
+	cout<< y <<endl;
+
+	/*const int cx = 7;
 	const int cy = 9;
-
-	cout << "x=" << cx << " y=" << cy << endl;
-
 	swap_v(cx,cy);
+	
+	cout<< cx <<endl;
+	cout<< cy <<endl;*/
 
-	cout << "x=" << cx << " y=" << cy << endl;
+	swap_d1(7.7,9.9);
 
-	swap_v(7.7,9.9);
-
+	
+	
 	double dx = 7.7;
 	double dy = 9.9;
+	swap_d(dx,dy);
 
-	cout << "x=" << dx << " y=" << dy << endl;
+	cout<< dx <<endl;
+	cout<< dy <<endl;
 
-	swap_v(dx,dy);
+	
+	swap_d1(7.7,9.9);
 
-	cout << "x=" << dx << " y=" << dy << endl;
+	cout<< dx <<endl;
+	cout<< dy <<endl;
 
-	swap_v(7.7,9.9);
+
 
 	return 0;
-	}
+}
--- drills/drill8/use.cpp
+++ drills/drill8/use.cpp
@@ -1,5 +1,5 @@
 #include "my.h"
-#include <iostream>
+
 
 int foo;
 
